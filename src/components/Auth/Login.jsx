7714e4865753cf6fd55dad94f7504d06e8cac12e
yashapex <yg21260@gmail.com>
2025-07-05 22:24:13 +0530

    Login page update

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import App from '../../App'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import { faRightToBracket } from '@fortawesome/free-solid-svg-icons'
 
 
 const Login = ({handlelogin}) => {
@@ -26,11 +27,25 @@ const Login = ({handlelogin}) => {
 
     
   return (
-    <div className='flex h-screen w-screen items-center justify-center'>
-      <div className=' h-3/4 border border-emerald-800 rounded-2xl bg-[#1c1c1c] flex flex-col'>
-        <h2 className='text-3xl font-semibold m-4'>Log In</h2>
-        <div className='flex-1 flex items-center justify-center p-5'>
+    
+    <div  className='relative bg-center bg-cover m-0 flex h-screen w-full'
+          style={{ backgroundImage: "url('bg2.jpg') "}}
+    >
 
+    {/* Overlay for dimming the background */}
+    <div className='absolute inset-0 bg-black/60 z-0'></div>
+
+    {/* Foreground login box */}
+    <div className="relative z-10 flex items-center justify-center h-full w-full">
+
+        <div className="h-[75%] w-[80%] max-w-2xl bg-[#1c1c1c] border border-emerald-800 rounded-2xl p-10 shadow-lg flex flex-col items-center justify-center gap-16 
+                        fade-up ">
+
+          {/* Heading */}
+          <h2 className="text-5xl font-poppins font-bold">Log In <FontAwesomeIcon icon={faRightToBracket} className="text-4xl text-emerald-500" />
+          </h2>
+
+      <div className='flex-1 flex items-center justify-center p-5'>
         <form 
             onSubmit={submitHandler}
             className='flex flex-col items-center justify-center' 
@@ -42,7 +57,15 @@ const Login = ({handlelogin}) => {
             onChange={(e) => {
               setEmail(e.target.value);
             }}
-            className='w-80 border-1 border-emerald-600 rounded-full py-3 px-5 ' 
+            className=' w-80 
+                        border-1 border-emerald-600 rounded-full 
+                        py-3 px-5
+                        focus:outline-none
+                        focus:border-emerald-400 
+                        focus:ring-2 
+                        focus:ring-emerald-300 
+                        focus:shadow-[0_0_8px_2px_rgba(16,185,129,0.6)]
+                        transition-all duration-300 ' 
             placeholder='👤Email'
             autoComplete="email" 
         />
@@ -55,17 +78,34 @@ const Login = ({handlelogin}) => {
             onChange={(e) => {
               setPassword(e.target.value);
             }}
-            className='w-80 border-1 border-emerald-600 rounded-full py-3 px-5 mt-5' 
+            className=' w-80 
+                        border-1 border-emerald-600 rounded-full 
+                        py-3 px-5 mt-5
+                        focus:outline-none
+                        focus:border-emerald-400 
+                        focus:ring-2 
+                        focus:ring-emerald-300 
+                        focus:shadow-[0_0_8px_2px_rgba(16,185,129,0.6)]
+                        transition-all duration-300 ' 
             placeholder='🔒Password'
             
         />
         <FontAwesomeIcon 
           icon={showPassword ? faEyeSlash :faEye}
           onClick={togglePassword} 
-          className=' absolute text-gray-500 right-2 top-[65%] transform -translate-y-1/2 ' />
+          className=' absolute text-gray-500 right-2 top-[65%] transform -translate-y-1/2 ' 
+        />
         </div>
         <button
-          className=' w-full  cursor-pointer rounded-3xl mt-5 px-5 py-2 bg-emerald-800 '
+          className=' w-full cursor-pointer font-medium
+                      rounded-3xl 
+                      mt-5 
+                      px-5 py-2 
+                      bg-emerald-800
+                      hover:bg-emerald-600
+                      active:scale-95                         
+                      transition-all duration-200 
+                      ease-in-out '
         >
         Log in
         </button>
@@ -74,6 +114,7 @@ const Login = ({handlelogin}) => {
         
         </div>
       </div>
+    </div>
     </div>
   )
 }
